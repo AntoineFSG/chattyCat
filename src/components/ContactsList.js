2bfe85cdd2516b1a809fc51cb57ac6b98d2f25ad
Antoine Roy <antoineroy@Antoines-MBP-15.attlocal.net>
2020-06-05 12:21:39 -0700

    init

---
+++ src/components/ContactsList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Contact } from "./Contact";
+
+const ContactsList = ({ contacts }) => {
+  console.log("CONTACTLIST: " + contacts);
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          width: "auto",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center"
+        }}
+      >
+        {contacts &&
+          contacts[0] !== null &&
+          contacts.map(c => <Contact key={c + "key"} contactUID={c} />)}
+        {!contacts && <p>loading...</p>}
+      </div>
+    </div>
+  );
+};
+export default ContactsList;
